3652b2ac2b8ec0745c4c9917a034483868f46b40
Anqi Qu <aqu@bscglobal.com>
2020-08-09 22:29:37 +0200

    //WIP - starts building the business day calculator component

--- src/app/components/business-day-calculator/business-day-calulator.component.ts
+++ src/app/components/business-day-calculator/business-day-calulator.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import * as data from "../../data/business-hours.json";
+import { DayOfWeek } from "../../enums/day-of-week";
+import { PublicHoliday } from "../../interfaces/public-holiday.interface";
 
 @Component({
   selector: 'business-day-calculator',
@@ -8,7 +12,66 @@ import { Component } from '@angular/core';
 
 export class BusinessDayCalculatorComponent {
 
+  @ViewChild('picker') picker: any;
+
+  dateControl = new FormControl(new Date()); // Sets the default date to now
+  disabled = false;
+  message: string = "";
+  isBusinessHours: boolean;
+
   constructor() {
+  }
+
+  calculateBusinessHours(date: Date) {
+    if (!this.isWorkDay(date)) {
+      this.message = "We are not open :(";
+      this.isBusinessHours = false;
+    } else if (this.isPublicHoliday(date)) {
+      let holiday: any = this.isPublicHoliday(date);
+      this.message = "We are not open. Today is " + holiday.name;
+      this.isBusinessHours = false;
+    } else if (this.isBreak(date)) {
+      this.message = "We are open but just on a break";
+      this.isBusinessHours = false;
+    } else if (this.isOpen(date)) {
+      this.message = "We are open!";
+      this.isBusinessHours = true;
+    } else {
+      throw new Error("Oops, an error occured. Please check your input and try again");
+    }
+  }
+
+  isWorkDay(date: Date): boolean {
+    let workdays: number[] = data.work_week.map(day => {
+      return DayOfWeek[day]
+    });
+
+    return workdays.includes(date.getDay()) ? true : false;
+  }
+
+  isPublicHoliday(date: Date): PublicHoliday | false {
+    var Holidays = require('date-holidays')
+    let publicHolidays = new Holidays()
+    publicHolidays.init('ZA')
+
+    if (!publicHolidays.isHoliday(date)) {
+      return false;
+    } else {
+      let holiday: PublicHoliday;
+      return Object.assign(holiday, publicHolidays.isHoliday(date));
+    }
+  }
+
+  isBreak(date: Date): boolean {
+    
+  }
+
+  isOpen(date: Date): boolean {
+
+  }
+
+  getNextOpenTime(date: Date): string {
+
   }
 
 }
---
+++ src/app/enums/day-of-week.ts
@@ -0,0 +1,11 @@
+import { WeekDay } from '@angular/common';
+
+export enum DayOfWeek {
+    sun,
+    mon,
+    tue,
+    wed,
+    thu,
+    fri,
+    sat
+  }
