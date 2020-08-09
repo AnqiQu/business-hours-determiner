f59703e83e8a538865f0cc28757d3025e1935020
Anqi Qu <aqu@bscglobal.com>
2020-08-09 20:40:04 +0200

    Adds business day calculator component

---
+++ src/app/components/business-day-calculator/business-day-calulator.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'business-day-calculator',
+  templateUrl: './business-day-calculator.component.html',
+  styleUrls: ['./business-day-calculator.component.scss']
+})
+
+export class BusinessDayCalculatorComponent {
+
+  constructor() {
+  }
+
+}
